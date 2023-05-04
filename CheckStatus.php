389ed6ffd61e748ce957d79ae0c1f747e972bf80
Toxes Foxes <toxes_foxes@outlook.com>
2023-05-04 17:10:30 +0600

    Update CheckStatus.php

--- CheckStatus.php
+++ CheckStatus.php
@@ -1,6 +1,6 @@
 <?php
 
-function check_status($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $CPU_ALERT, $RAM_ALERT, $DISK_ALERT){
+function check_status($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $CPU_ALERT, $RAM_ALERT, $DISK_ALERT) {
     $cpu = intval(shell_exec("top -b -n1 | grep 'Cpu(s)' | awk '{print $2}'"));
     $ram = intval(shell_exec("free | grep Mem | awk '{print $3/$2 * 100.0}'"));
     $disk = shell_exec("df -h / | awk '{print $5}' | sed 's/%//;s/\\n/ /'");
@@ -8,16 +8,16 @@ function check_status($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $CPU_ALERT, $RAM_ALERT, 
 
     if ($cpu > $CPU_ALERT || $ram > $RAM_ALERT || $disk > $DISK_ALERT) {
         $message = "ALERT: Resource usage is high!\n"
-                . "CPU: $cpu%,\n"
-                . "RAM: $ram%,\n"
-                . "Disk: $disk%";
-        if(!sendHtopAlert($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $message)){
+            . "CPU: $cpu%,\n"
+            . "RAM: $ram%,\n"
+            . "Disk: $disk%";
+        if (!sendFullAlert($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $message)) {
             sendMessage($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $message);
         }
     }
 }
 
-function sendMessage($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $message){
+function sendMessage($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $message) {
     $data = array(
         'chat_id' => $CHAT_ID,
         'text' => $message,
@@ -38,28 +38,61 @@ function sendMessage($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $message){
     $result = file_get_contents($url, false, $context);
 }
 
-function sendHtopAlert($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $message) {
+#region htop
+function writeHtop() {
+    shell_exec("export TERM=xterm-color COLUMNS=120 LINES=21;echo q | htop | aha --black --line-fix > htop.html");
+}
+function sendHtopImage($TOKEN, $CHAT_ID,$TOPIC_CHAT_ID, $message){
+    $URL = "https://api.telegram.org/bot$TOKEN/sendPhoto";
+    $POST = array(
+        'chat_id' => $CHAT_ID,
+        'photo' => new CURLFile(realpath("./htop.png")),
+        'caption' => $message,
+        'disable_notification' => true,
+        'reply_to_message_id' => $TOPIC_CHAT_ID
+    );
+    $CH = curl_init();
+    curl_setopt($CH, CURLOPT_URL, $URL);
+    curl_setopt($CH, CURLOPT_POST, 1);
+    curl_setopt($CH, CURLOPT_POSTFIELDS, $POST);
+    curl_setopt($CH, CURLOPT_RETURNTRANSFER, 1);
+    curl_exec($CH);
+    curl_close($CH);
+}
+#endregion
+
+#region ps
+function writeLastProcessesCMDs($count = 4, $header = "--no-header") {
+    $count = intval($count) + 1;
+    shell_exec("export TERM=xterm-color;ps -eo pid,%cpu,%mem,cmd --sort=-%cpu | head -n $count | aha --black " . $header . " > pinfos.html");
+}
+function sendProcessesInfo($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID) {
+    $URL = "https://api.telegram.org/bot$TOKEN/sendDocument";
+    $POST = array(
+        'chat_id' => $CHAT_ID,
+        'document' => new CURLFile(realpath("./pinfos.html")),
+        'caption' => "Last 10 processes",
+        'disable_notification' => true,
+        'reply_to_message_id' => $TOPIC_CHAT_ID
+    );
+    $CH = curl_init();
+    curl_setopt($CH, CURLOPT_URL, $URL);
+    curl_setopt($CH, CURLOPT_POST, 1);
+    curl_setopt($CH, CURLOPT_POSTFIELDS, $POST);
+    curl_setopt($CH, CURLOPT_RETURNTRANSFER, 1);
+    curl_exec($CH);
+    curl_close($CH);
+}
+#endregion
+
+function sendFullAlert($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $message) {
     $wkhtmltoimage = shell_exec('which wkhtmltoimage');
     if (strpos($wkhtmltoimage, 'wkhtmltoimage') !== false) {
-        $htop_output = shell_exec("export TERM=xterm-color;export COLUMNS=120 LINES=14;echo q | htop | aha --black --line-fix > htop.html");
-        $htop_output = file_get_contents('./htop.html');
+        writeHtop();
+        writeLastProcessesCMDs(10, "");
         exec("wkhtmltoimage ./htop.html ./htop.png");
-
-        $URL = "https://api.telegram.org/bot$TOKEN/sendPhoto";
-        $POST = array(
-            'chat_id' => $CHAT_ID,
-            'photo' => new CURLFile(realpath("./htop.png")),
-            'caption' => $message,
-            'disable_notification' => true,
-            'reply_to_message_id' => $TOPIC_CHAT_ID
-        );
-        $CH = curl_init();
-        curl_setopt($CH, CURLOPT_URL, $URL);
-        curl_setopt($CH, CURLOPT_POST, 1);
-        curl_setopt($CH, CURLOPT_POSTFIELDS, $POST);
-        curl_setopt($CH, CURLOPT_RETURNTRANSFER, 1);
-        $RESULT = curl_exec($CH);
-        curl_close($CH);
+        sendHtopImage($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID, $message);
+        sendProcessesInfo($TOKEN, $CHAT_ID, $TOPIC_CHAT_ID);
         return true;
     } else {
         echo "wkhtmltoimage is not installed";
